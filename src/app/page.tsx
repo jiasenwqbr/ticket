177f71ea34aa8b7f5c396d8b5e86450dad062344
jiasenwqbr <jasonjiasen@gmail.com>
2024-06-03 15:02:40 +0800

    reponse flex layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@ import { SignUp } from "@clerk/nextjs";
 import { UserButton, useAuth, useUser } from "@clerk/nextjs";
 import { Button } from "@nextui-org/button";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
+import { Spacer } from "@nextui-org/react";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ThemeSwitcher />
-      <UserButton />
-      <div>
-        <Button>Click me</Button>
+    <div>
+      <header className="w-full h-14">
+        <div className="fixed top-4 right-8 flex justify-items-stretch items-center">
+          <ThemeSwitcher />
+          <Spacer x={4} />
+          <UserButton />
+        </div>
+      </header>
+      <div className="flex items-center justify-center m-4">
+        <main className="flex flex-col items-center justify-center w-full boder-x-2 sm:w-full md:w-9/12 lg:w-6/12"></main>
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,14 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemeProvider attribute="class" defaultTheme="dark">
+      <NextThemesProvider attribute="class" defaultTheme="dark">
         {children}
-      </NextThemeProvider>
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
